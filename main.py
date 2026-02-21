eb27668a81eb7a7b800b71fba66a3f22a3c062d9
ArchWizardSlayne <33710341+ArchWizardSlayne@users.noreply.github.com>
2026-02-21 13:07:20 -0500

    Added continuous calculation loop. added reset command

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 def main():
     print("Welcome to Terminal Calculator!")
-    print("Enter 'quit' to exit\n")
+    print("Enter 'quit' to exit or 'reset' to reset the calculation\n")
     
     while True:
+        reset = False
+
+
         expression = input("Enter calculation: ")
         
 
         if expression.lower() == 'quit':
             print("Goodbye!")
-            break
+            quit()
 
 
         parts = expression.split()
@@ -23,13 +26,42 @@ def main():
         elif parts[1] == "-": result = num1 - num2 
         elif parts[1] == "*" or parts[1] == "x": result = num1 * num2 
         elif parts[1] == "/":
-            if num1 == 0 or num2 == 0:
+            if num2 == 0:
                 print("Cannot devide by 0")
                 continue 
             result = num1 / num2 
         else: print("Unknown operator")
 
         print(round(result, 2))
+        
+
+        while True:
+            expression = input()
+
+            if expression.lower() == "quit":
+                print("Goodbye!")
+                quit()
+            elif expression.lower() == "reset":
+                break
+
+            parts = expression.split()
+            if len(parts) != 2:
+                print("Try Again")
+                continue
+            num1 = float(result)
+            num2 = float(parts[1])
+
+            if parts[0] == "+": result = num1 + num2 
+            elif parts[0] == "-": result = num1 - num2 
+            elif parts[0] == "*" or parts[0] == "x": result = num1 * num2 
+            elif parts[0] == "/":
+                if num2 == 0:
+                    print("Cannot devide by 0")
+                    continue 
+                result = num1 / num2 
+            else: print("Unknown operator")
+
+            print(round(result, 2))
 
 if __name__ == "__main__":
     main()
